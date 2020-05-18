c571852ea4f67e7c6a286c20bce760e071981b10
hayami226 <59946552+hayami226@users.noreply.github.com>
2020-05-18 22:11:48 +0900

    Update Question02.java

--- Question02.java
+++ Question02.java
@@ -3,13 +3,13 @@
 
 class Solution02 {
     public int[] solution(int[] A, int K) {
-    	if( A.length == K )
+    	if(A.length == K)
     		return A;
     	
     	for(int i = 0; i < K; i++) {
     		int[] tmp = new int[A.length];
     		for(int j = 0; j < A.length; j++) 
-    			tmp[j] = ( j == 0 ) ? A[A.length-1] : A[j - 1];
+    			tmp[j] = (j == 0) ? A[A.length-1] : A[j - 1];
     		A = tmp;
     	}
     	return A;
@@ -24,6 +24,6 @@ public class Question02 {
     	int[] result = sol.solution(A, K);
     	
     	for(int num : result)
-    		System.out.println( num );
+    		System.out.println(num);
     }
 }
