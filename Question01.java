a39844c0549e8feee21d196abf60cb7830e73732
hayami226 <59946552+hayami226@users.noreply.github.com>
2020-05-18 14:11:05 +0900

    Add files via upload
    https://app.codility.com/demo/results/trainingNB29YA-NP9/

---
+++ Question01.java
@@ -0,0 +1,39 @@
+
+//https://app.codility.com/demo/results/trainingNB29YA-NP9/
+
+class Solution {
+    public int solution(int N) {
+        String binary = Integer.toBinaryString(N);
+        int max = 0;
+        for(int i = 0; i < binary.length(); i++) {
+        	String str = gerString(binary, i);
+        	if(str.equals("1")) {
+                boolean visible = false;
+                int iCnt = 0;
+        		for(int j = i; j < binary.length() - 1; j++) {
+        			if(gerString(binary, j + 1).equals("0"))
+        				iCnt++;
+        			else {
+        				visible = true;
+        				break;
+        			}
+        		}
+    			if( visible && max < iCnt ) {
+    				max = iCnt;
+    			}
+        	}
+        }
+        return max;
+    }
+    
+    public String gerString(String str, int idx) {
+    	return String.valueOf(str.charAt(idx));
+    }
+}
+
+public class Question01 {
+    public static void main(String args[]){
+    	Solution sol = new Solution();
+    	System.out.println( String.valueOf(sol.solution(10000)) );
+    }
+}
