6f930f1feed550a3c62a3a8af3d1bdd5ffbd2c9e
hayami226 <59946552+hayami226@users.noreply.github.com>
2020-05-19 22:44:40 +0900

    Update Lesson3_FrogJmp.java

--- Lesson3_FrogJmp.java
+++ Lesson3_FrogJmp.java
@@ -4,10 +4,3 @@ class Solution04 {
         return (int)Math.ceil((double)(Y - X)/D);
     }
 }
-
-public class Lesson3_FrogJmp {
-    public static void main(String args[]){
-    	Solution04 sol = new Solution04();
-    	System.out.println(sol.solution(10,85,30));
-    }
-}
