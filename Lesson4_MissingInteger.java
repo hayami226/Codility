d544eb47cc0d9b56b343ae2952971b19477ccead
hayami226 <59946552+hayami226@users.noreply.github.com>
2020-05-29 16:08:48 +0900

    Create Lesson4_MissingInteger.java

---
+++ Lesson4_MissingInteger.java
@@ -0,0 +1,17 @@
+//https://app.codility.com/demo/results/trainingT8R2GS-4N4/
+import java.util.*;
+class Solution02 {
+	public int solution(int[] A) {
+		Arrays.sort(A);		
+		if(Arrays.binarySearch(A, 1) < 0)
+			return 1;
+		
+		for(int i = 0; i < A.length - 1; i++) {		
+			if(A[i] < 0 || A[i] == A[i + 1] || A[i] + 1 == A[i + 1])
+				continue;
+				
+			return A[i] + 1;
+		}		
+		return A[A.length - 1] + 1;
+	}
+}
